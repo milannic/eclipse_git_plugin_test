d2139e2be093078036e63388e04746f97e1c5c23
Milannic <milannic.cheng.liu@gmail.com>
2014-08-19 21:52:50 -0400

    3rd

--- src/com/milannic/HelloWorld.java
+++ src/com/milannic/HelloWorld.java
@@ -3,6 +3,7 @@ package com.milannic;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Second Push\n");
+		System.out.println("Third Push\n");
 		System.out.println("Hello World\n");
 	}
 
